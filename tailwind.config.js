645509fba286b1b88cecfb67f538a60080031aad
Novitz Jean-Philippe <novitz@gmail.com>
2026-01-12 02:39:01 +0100

    update to symfony 7.4.3 Add header and footer templates; update base layout and Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,32 +5,12 @@ module.exports = {
     "./templates/**/*.html.twig",
   ],
   theme: {
-    extend: {
-      transitionProperty: {
-        'spacing': 'margin, padding, display',
-        'width': 'width',
-      },
-      colors: {
-        "transparent": "transparent",
-        "white": "#F8F9FA",
-        "base": {
-          "light": "#FFFFFF", // Blanc pur pour le fond en mode clair
-          "dark": "#121212",  // Noir profond pour le fond en mode sombre
-        },
-        "surface": {
-          "light": "#F5F5F5",
-          "dark": "#1E1E1E",
-          "secondary": "#B0B0B0",
-        },
-        "content": {
-          "primary": {
-            "light": "#333333",
-            "dark": "#E0E0E0",
-          },
-          "secondary": "#8C8C8C",
-        },
-      },
-    },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"], // personnalisez selon vos besoins
+  },
 }
